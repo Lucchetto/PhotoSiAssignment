befa019fd1055ed162a492cc31c6196c9fb8ec11
Lucchetto <lucchetto.tie@live.com>
2025-01-18 16:18:45 +0100

    Replace value class with data in RepoApiErrorEntity Result
        * Inlined classes break PreviewParameterProvider
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- domain/src/main/java/com/photosi/assignment/domain/entity/RepoApiErrorEntity.kt
+++ domain/src/main/java/com/photosi/assignment/domain/entity/RepoApiErrorEntity.kt
@@ -1,15 +1,10 @@
 package com.photosi.assignment.domain.entity
 
-import java.io.IOException
-
 sealed interface RepoApiErrorEntity<out CustomError> {
 
-    @JvmInline
-    value class Custom<CustomError>(val e: CustomError): RepoApiErrorEntity<CustomError>
+    data class Custom<CustomError>(val e: CustomError): RepoApiErrorEntity<CustomError>
 
-    @JvmInline
-    value class Network(val e: Throwable): RepoApiErrorEntity<Nothing>
+    data class Network(val e: Throwable): RepoApiErrorEntity<Nothing>
 
-    @JvmInline
-    value class Unknown(val e: Throwable): RepoApiErrorEntity<Nothing>
+    data class Unknown(val e: Throwable): RepoApiErrorEntity<Nothing>
 }
--- domain/src/main/java/com/photosi/assignment/domain/entity/Result.kt
+++ domain/src/main/java/com/photosi/assignment/domain/entity/Result.kt
@@ -2,9 +2,7 @@ package com.photosi.assignment.domain.entity
 
 sealed interface Result<T, E> {
 
-    @JvmInline
-    value class Success<T, E>(val value: T): Result<T, E>
+    data class Success<T, E>(val value: T): Result<T, E>
 
-    @JvmInline
-    value class Failure<T, E>(val error: E): Result<T, E>
+    data class Failure<T, E>(val error: E): Result<T, E>
 }
