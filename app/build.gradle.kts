74207a41142e3e42426af58be329c2cb6fc6989a
Lucchetto <lucchetto.tie@live.com>
2025-01-20 22:35:10 +0100

    Version 1.0.4
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.photosi.assignment"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
